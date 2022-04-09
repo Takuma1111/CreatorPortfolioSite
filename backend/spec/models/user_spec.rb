8b165bb9ab1de563344cee49cc2487b552f37eec
takuma <takuma012011@gmail.com>
2022-04-09 16:13:19 +0900

    [fix] ファイルpush

---
+++ backend/spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a email and password" do
+    user = User.new(
+        name: "hoge1",
+        email: "hogehoge@gmail.com",
+        password: "password"
+    )
+    expect(user).to be_valid
+  end
+  it "is invalid without a name" do
+    user = User.new(
+      email: "hogehoge@gmail.com",
+      password: "password"
+    )
+    expect(user).not_to eq be_valid
+  end
+  it "is invalid without an email address" do
+    user = User.new(
+      name: "hoge1",
+      password: "password"
+    )
+    expect(user).not_to eq be_valid
+  end
+  it "is invalid without a duplicate email address" do
+    user = User.new(
+      name: "hoge1",
+      email: "hogehoge@gmail.com",
+      password: "password"
+    )
+    expect(user["email"]).not_to eq "hogehoge@gmail.com"
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+end
